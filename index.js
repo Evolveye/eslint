847c417f65f956a9646b59e4c626fefa97e3a515
Evolveye <stolarski.paw@gmail.com>
2021-02-14 17:45:02 +0100

    Reinventing the wheel again

---
+++ index.js
@@ -0,0 +1,24 @@
+require( `./custom_rules/tests.js` )
+
+const something = []
+
+const arrow_params = ({ abc }, a) => {
+}
+/*
+const arrow_obj = ({ abc }) => true
+const arrow_empty = () => true
+const arrow_var = a => true
+const arrow = (a, b) => true
+
+if (something[ 0 ]);
+if (arrow());
+
+for (const value of [ 1, 2, 3 ]);
+
+function fnObj({ c }) {}
+function fn( a, b, { c }) {}
+
+fnObj({ c:3 })
+fn( 1, 2, { c:3 } )
+fn( 1, 2, something[ 2 ] )
+// */
